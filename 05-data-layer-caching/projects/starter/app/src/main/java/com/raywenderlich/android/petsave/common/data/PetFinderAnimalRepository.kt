3fc4ba266426b5af473c46540069669acf031a7f
Godday Okoduwa <goddayokos@gmail.com>
2021-09-28 20:22:09 +0100

    Implemented repository for the data layer

---
+++ 05-data-layer-caching/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/common/data/PetFinderAnimalRepository.kt
@@ -0,0 +1,73 @@
+package com.raywenderlich.android.petsave.common.data
+
+import com.raywenderlich.android.petsave.common.data.api.PetFinderApi
+import com.raywenderlich.android.petsave.common.data.api.model.mappers.ApiAnimalMapper
+import com.raywenderlich.android.petsave.common.data.api.model.mappers.ApiPaginationMapper
+import com.raywenderlich.android.petsave.common.data.cache.Cache
+import com.raywenderlich.android.petsave.common.data.cache.model.cachedanimal.CachedAnimalAggregate
+import com.raywenderlich.android.petsave.common.data.cache.model.cachedorganization.CachedOrganization
+import com.raywenderlich.android.petsave.common.domain.model.animal.Animal
+import com.raywenderlich.android.petsave.common.domain.model.animal.details.AnimalWithDetails
+import com.raywenderlich.android.petsave.common.domain.model.pagination.PaginatedAnimals
+import com.raywenderlich.android.petsave.common.domain.repositories.AnimalRepository
+import io.reactivex.Flowable
+import javax.inject.Inject
+
+class PetFinderAnimalRepository @Inject constructor(
+    private val api: PetFinderApi,
+    private val cache: Cache,
+    private val apiAnimalMapper: ApiAnimalMapper,
+    private val apiPaginationMapper: ApiPaginationMapper
+) : AnimalRepository {
+
+    private val postcode = "07097"
+    private val maxDistanceMiles = 100
+
+    override fun getAnimals(): Flowable<List<Animal>> {
+        return cache.getNearbyAnimals()
+            .distinctUntilChanged()
+            .map { animalList ->
+                animalList.map {
+                    it.animal.toAnimalDomain(
+                        it.photos,
+                        it.videos,
+                        it.tags
+                    )
+                }
+            }
+    }
+
+    override suspend fun requestMoreAnimals(pageToLoad: Int, numberOfItems: Int): PaginatedAnimals {
+       val (apiAnimals, apiPagination) = api.getNearbyAnimals(
+          pageToLoad,
+          numberOfItems,
+          postcode,
+          maxDistanceMiles
+       )
+
+        return PaginatedAnimals(
+            apiAnimals?.map {
+                apiAnimalMapper.mapToDomain(it)
+            }.orEmpty(),
+            apiPaginationMapper.mapToDomain(apiPagination)
+        )
+    }
+
+    override suspend fun storeAnimals(animals: List<AnimalWithDetails>) {
+        val organizations = animals.map {
+            CachedOrganization.fromDomain(it.details.organization)
+        }
+        /*
+            Organizations has a one-to-many relationship with animals
+            so we have to insert them before inserting animals.
+            Otherwise, Room will complain about not being able to satisfy
+            the foreign key’s constraint in CachedAnimalWithDetails.
+         */
+        cache.storeOrganizations(organizations)
+        // Insert animals
+        cache.storeNearbyAnimals(animals.map {
+            CachedAnimalAggregate.fromDomain(it)
+        })
+
+    }
+}
