ce90783b2e89b826a2cd929b5c35b0bb91b04f21
Godday Okoduwa <goddayokos@gmail.com>
2021-09-28 19:07:38 +0100

    Manage room database dependencies with hilt

--- 05-data-layer-caching/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/common/data/di/CacheModule.kt
+++ 05-data-layer-caching/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/common/data/di/CacheModule.kt
@@ -34,21 +34,40 @@
 
 package com.raywenderlich.android.petsave.common.data.di
 
+import android.content.Context
+import androidx.room.Room
 import com.raywenderlich.android.petsave.common.data.cache.PetSaveDatabase
+import com.raywenderlich.android.petsave.common.data.cache.daos.AnimalsDao
 import com.raywenderlich.android.petsave.common.data.cache.daos.OrganizationsDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class CacheModule {
 
-  companion object {
+    companion object {
 
-    @Provides
-    fun provideOrganizationsDao(petSaveDatabase: PetSaveDatabase): OrganizationsDao =
-        petSaveDatabase.organizationsDao()
-  }
+        @Provides
+        @Singleton
+        fun provideDatabase(@ApplicationContext context: Context): PetSaveDatabase {
+            return Room.databaseBuilder(
+                context,
+                PetSaveDatabase::class.java,
+                "petsave.db"
+            ).build()
+        }
+
+        @Provides
+        fun provideAnimalsDao(petSaveDatabase: PetSaveDatabase): AnimalsDao =
+            petSaveDatabase.animalsDao()
+
+        @Provides
+        fun provideOrganizationsDao(petSaveDatabase: PetSaveDatabase): OrganizationsDao =
+            petSaveDatabase.organizationsDao()
+    }
 }
