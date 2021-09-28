7e4f7d3348dffb018a59b8e840012d2f27daf9cb
Godday Okoduwa <goddayokos@gmail.com>
2021-09-28 23:33:01 +0100

    Added the setup and teardown test methods

--- 05-data-layer-caching/projects/starter/app/src/androidTest/java/com/raywenderlich/android/petsave/common/data/PetFinderAnimalRepositoryTest.kt
+++ 05-data-layer-caching/projects/starter/app/src/androidTest/java/com/raywenderlich/android/petsave/common/data/PetFinderAnimalRepositoryTest.kt
@@ -5,13 +5,18 @@ import com.raywenderlich.android.petsave.common.data.api.model.mappers.ApiAnimal
 import com.raywenderlich.android.petsave.common.data.api.model.mappers.ApiPaginationMapper
 import com.raywenderlich.android.petsave.common.data.api.utils.FakeServer
 import com.raywenderlich.android.petsave.common.data.cache.Cache
+import com.raywenderlich.android.petsave.common.data.preferences.FakePreferences
 import com.raywenderlich.android.petsave.common.data.preferences.Preferences
 import com.raywenderlich.android.petsave.common.domain.repositories.AnimalRepository
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
+import org.threeten.bp.Instant
 import retrofit2.Retrofit
 import javax.inject.Inject
 
@@ -33,4 +38,42 @@ class PetFinderAnimalRepositoryTest {
     lateinit var apiAnimalMapper: ApiAnimalMapper
     @Inject
     lateinit var apiPaginationMapper: ApiPaginationMapper
+
+    @BindValue
+    @JvmField
+    val preferences: Preferences = FakePreferences()
+
+    @Before
+    fun setup() {
+        fakeServer.start()   // Start the fake server
+
+        // Setup preferences
+        preferences.apply {
+            deleteTokenInfo()
+            putToken("validToken")
+            putTokenExpirationTime(
+                Instant.now().plusSeconds(3600).epochSecond
+            )
+            putTokenType("Bearer")
+        }
+
+        hiltRule.inject()  // Tell hilt to inject the dependencies
+
+        api = retrofitBuilder
+            .baseUrl(fakeServer.baseEndpoint)
+            .build()
+            .create(PetFinderApi::class.java)
+
+        repository = PetFinderAnimalRepository(
+            api,
+            cache,
+            apiAnimalMapper,
+            apiPaginationMapper
+        )
+    }
+
+    @After
+    fun teardown() {
+        fakeServer.shutdown()
+    }
 }
