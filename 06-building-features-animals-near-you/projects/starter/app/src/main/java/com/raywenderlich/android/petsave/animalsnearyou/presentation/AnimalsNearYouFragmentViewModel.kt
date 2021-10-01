8abf7e4ee923e2097804a5cc1f42c04debe08144
Godday Okoduwa <goddayokos@gmail.com>
2021-10-01 21:44:40 +0100

    Added logic for state management to the ViewModel

--- 06-building-features-animals-near-you/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/animalsnearyou/presentation/AnimalsNearYouFragmentViewModel.kt
+++ 06-building-features-animals-near-you/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/animalsnearyou/presentation/AnimalsNearYouFragmentViewModel.kt
@@ -1,9 +1,17 @@
 package com.raywenderlich.android.petsave.animalsnearyou.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.raywenderlich.android.petsave.common.domain.model.NetworkException
+import com.raywenderlich.android.petsave.common.domain.model.NetworkUnavailableException
+import com.raywenderlich.android.petsave.common.presentation.Event
 import com.raywenderlich.android.petsave.common.presentation.model.mappers.UiAnimalMapper
 import com.raywenderlich.android.petsave.common.utils.DispatchersProvider
+import com.raywenderlich.android.petsave.common.utils.createExceptionHandler
 import io.reactivex.disposables.CompositeDisposable
+import kotlinx.coroutines.launch
 
 class AnimalsNearYouFragmentViewModel constructor(
     private val uiAnimalMapper: UiAnimalMapper,
@@ -11,8 +19,50 @@ class AnimalsNearYouFragmentViewModel constructor(
     private val compositeDisposable: CompositeDisposable
 ) : ViewModel() {
 
+    val state: LiveData<AnimalsNearYouViewState> get() = _state
+    private val _state = MutableLiveData<AnimalsNearYouViewState>()
+    private var currentPage = 0
+
+    init {
+        _state.value = AnimalsNearYouViewState()
+    }
+
     override fun onCleared() {
         super.onCleared()
         compositeDisposable.clear()
+    }
+
+    fun onEvent(event: AnimalNearYouEvent) {
+        when (event) {
+            is AnimalNearYouEvent.RequestInitialAnimalsList -> loadAnimals()
+        }
+    }
+
+    private fun loadAnimals() {
+        if (state.value!!.animals.isEmpty()) {
+            loadNextAnimalPage()
+        }
+    }
+
+    private fun loadNextAnimalPage() {
+        val errorMessage = "Failed to fetch nearby animals"
+        val exceptionHandler =
+            viewModelScope.createExceptionHandler(errorMessage) { onFailure(it) }
+
+        viewModelScope.launch(exceptionHandler) {
+
+        }
+    }
+
+    private fun onFailure(failure: Throwable) {
+        when (failure) {
+            is NetworkException,
+                is NetworkUnavailableException -> {
+                    _state.value = state.value!!.copy(
+                        loading = false,
+                        failure = Event(failure)
+                    )
+                }
+        }
     }
 }
