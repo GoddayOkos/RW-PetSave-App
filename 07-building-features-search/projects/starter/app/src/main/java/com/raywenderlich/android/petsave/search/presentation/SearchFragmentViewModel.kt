10836811f3c9b5daca264f07e596cb79ebba7f6e
Godday Okoduwa <goddayokos@gmail.com>
2021-10-02 18:28:48 +0100

    Added state emanagement logic to the ViewModel

--- 07-building-features-search/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/search/presentation/SearchFragmentViewModel.kt
+++ 07-building-features-search/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/search/presentation/SearchFragmentViewModel.kt
@@ -49,6 +49,9 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.BehaviorSubject
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -58,72 +61,90 @@ class SearchFragmentViewModel @Inject constructor(
     private val compositeDisposable: CompositeDisposable
 ): ViewModel() {
 
-  val state: LiveData<SearchViewState> get() = _state
+    val state: LiveData<SearchViewState> get() = _state
 
-  private val _state: MutableLiveData<SearchViewState> = MutableLiveData()
-  private val querySubject = BehaviorSubject.create<String>()
-  private val ageSubject = BehaviorSubject.createDefault("")
-  private val typeSubject = BehaviorSubject.createDefault("")
+    private val _state: MutableLiveData<SearchViewState> = MutableLiveData()
+    private val querySubject = BehaviorSubject.create<String>()
+    private val ageSubject = BehaviorSubject.createDefault("")
+    private val typeSubject = BehaviorSubject.createDefault("")
 
-  private var currentPage = 0
+    private var currentPage = 0
 
-  init {
-    _state.value = SearchViewState()
-  }
-
-  fun onEvent(event: SearchEvent) {
-    when(event) {
-      is SearchEvent.PrepareForSearch -> prepareForSearch()
-      else -> onSearchParametersUpdate(event)
-    }
-  }
-
-  private fun onSearchParametersUpdate(event: SearchEvent) {
-    // Add code here
-  }
-
-  private fun prepareForSearch() {
-    // Add code here
-  }
-
-  private fun createExceptionHandler(message: String): CoroutineExceptionHandler {
-    return viewModelScope.createExceptionHandler(message) {
-      onFailure(it)
-    }
-  }
-
-  private fun setSearchingState() {
-    _state.value = state.value!!.updateToSearching()
-  }
-
-  private fun setNoSearchQueryState() {
-    _state.value = state.value!!.updateToNoSearchQuery()
-  }
-
-  private fun onAnimalList(animals: List<Animal>) {
-    _state.value =
-        state.value!!.updateToHasSearchResults(animals.map { uiAnimalMapper.mapToView(it) })
-  }
-
-  private fun resetPagination() {
-    currentPage = 0
-  }
-
-  private fun onPaginationInfoObtained(pagination: Pagination) {
-    currentPage = pagination.currentPage
-  }
-
-  private fun onFailure(throwable: Throwable) {
-    _state.value = if (throwable is NoMoreAnimalsException) {
-      state.value!!.updateToNoResultsAvailable()
-    } else {
-      state.value!!.updateToHasFailure(throwable)
+    init {
+        _state.value = SearchViewState()
     }
 
-  }
+    private fun loadFilterValues() {
+        val exceptionHandler =
+            createExceptionHandler(message = "Failed to get the filter values!")
+        viewModelScope.launch(exceptionHandler) {
+            val (ages, types) = withContext(dispatchersProvider.io()) {
+                getSearchFilters()
+            }
+            updateStateWithFilterValues(ages, types)
+        }
+    }
 
-  override fun onCleared() {
-    super.onCleared()
-    compositeDisposable.clear()
-  }
+    private fun updateStateWithFilterValues(
+        ages: List<String>,
+        types: List<String>
+    ) {
+        _state.value = state.value!!.updateToReadyToSearch(ages, types)
+    }
+
+    fun onEvent(event: SearchEvent) {
+        when(event) {
+            is SearchEvent.PrepareForSearch -> prepareForSearch()
+            else -> onSearchParametersUpdate(event)
+        }
+    }
+
+    private fun onSearchParametersUpdate(event: SearchEvent) {
+        // Add code here
+    }
+
+    private fun prepareForSearch() {
+        // Add code here
+    }
+
+    private fun createExceptionHandler(message: String): CoroutineExceptionHandler {
+        return viewModelScope.createExceptionHandler(message) {
+            onFailure(it)
+        }
+    }
+
+    private fun setSearchingState() {
+        _state.value = state.value!!.updateToSearching()
+    }
+
+    private fun setNoSearchQueryState() {
+        _state.value = state.value!!.updateToNoSearchQuery()
+    }
+
+    private fun onAnimalList(animals: List<Animal>) {
+        _state.value =
+            state.value!!.updateToHasSearchResults(animals.map { uiAnimalMapper.mapToView(it) })
+    }
+
+    private fun resetPagination() {
+        currentPage = 0
+    }
+
+    private fun onPaginationInfoObtained(pagination: Pagination) {
+        currentPage = pagination.currentPage
+    }
+
+    private fun onFailure(throwable: Throwable) {
+        _state.value = if (throwable is NoMoreAnimalsException) {
+            state.value!!.updateToNoResultsAvailable()
+        } else {
+            state.value!!.updateToHasFailure(throwable)
+        }
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
 }
